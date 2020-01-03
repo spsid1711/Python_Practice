f5a169aed2e553217bb35d03bca71fd0f20fc809
Siddharth Patel <spsid1711@gmail.com>
2020-01-03 08:39:10 -0500

    An exponential function has been created.

--- venv1/Exponent_function.py
+++ venv1/Exponent_function.py
@@ -1,3 +1,10 @@
 # In python, there already exists a function for exponential functions.
 print(2**3)
 # Lets try and create our own exponential function.
+def raise_to_pow(base, power):
+    result = 1
+    for i in range(power):
+        result *= base
+    return result
+
+print(raise_to_pow(2,3))
